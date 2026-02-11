050fbd5dd895a665998a044396525d280580f031
Melanie <qwtyopei@gmail.com>
2026-02-11 09:59:17 +0800

    fix: correct base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Reactclass-week6/",
+  base: "/Reactclass_week6/",
   plugins: [react()],
 })
